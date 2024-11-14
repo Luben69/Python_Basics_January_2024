d66d83a27f98fd16d96b05507582e4ae4e08c336
Luben69 <lpacharazov123@gmail.com>
2024-11-14 21:42:27 +0200

    Remove First_Steps_In_Coding_Exercises directory from repository

--- Python_Basics_January_2024/First_Steps_In_Coding_Exercises/Deposit_Calculator.py
+++
@@ -1,9 +0,0 @@
-dep_sum = float(input())
-dep_months = int(input())
-year_interest = float(input())
-
-year_interest1 = year_interest / 100
-
-final = dep_sum + dep_months * ((dep_sum * year_interest1) / 12)
-
-print(final)
